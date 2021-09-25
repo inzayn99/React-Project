b71787f7b0cfa98c5c163e2d3fb81380e616446c
Firoj <mymindhungry@gmail.com>
2021-09-26 01:34:29 +0545

    Day2-3

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,34 +1,37 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import web from "../src/Picture/logo.png"
+
 const Navbar = () => {
     return (
         <>
 
         <div className="container-fluid nav_bg">
         <div className="row">
-        <div className="col-10 mx-auto">
+        <div className="col-14 mx-auto">
         
        
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <NavLink className="navbar-brand" to="/">ZAYN</NavLink>
+    <NavLink className="zayn"  to="/"><img src={web} className="img-fluid animated" alt="" /></NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mb-2 mb-lg-0 m-xl-auto">
         <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+          <NavLink activeClassName='menu_active' exact className="nav-link active" aria-current="page" to="/">Home</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/about">About</NavLink>
+          <NavLink activeClassName='menu_active' className="nav-link" to="/about">About</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/contact">Contact</NavLink>
+          <NavLink activeClassName='menu_active' className="nav-link" to="/contact">Contact</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/service">Service</NavLink>
+          <NavLink activeClassName='menu_active' className="nav-link" to="/service">Service</NavLink>
         </li>
+        
       </ul>
       
     </div>
