44bed3b7456284b99572ce8c12bcbdff39b1ad9b
Firoj <mymindhungry@gmail.com>
2021-09-25 18:29:43 +0545

    Day2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import {Switch, Route } from 'react-router';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import {Switch, Route, Redirect } from 'react-router';
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
 import Service from "./Service";
+import Navbar from './Navbar';
 
 const App =() => {
   return ( 
     <>
+    <Navbar />
     <Switch>
     <Route exact path="/"  component={Home}     />
     <Route exact path="/contact"  component={Contact}     />
     <Route exact path="/about"  component={About}     />
     <Route exact path="/service"  component={Service}     />
+    <Redirect to="/" />
 
 
 
